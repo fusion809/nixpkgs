bf4e0a4bc0e2919ce39851db13d9aa177a75ec55
Brenton Horne <brentonhorne77@gmail.com>
2018-12-26 07:27:28 +1000

    openra-kknd: :arrow_up: 136

--- pkgs/games/openra-kknd/default.nix
+++ pkgs/games/openra-kknd/default.nix
@@ -10,7 +10,7 @@ with stdenv.lib;
 
 let
   pname = "openra-kknd";
-  version = "134";
+  version = "136";
   engine-version = "84936abee6d5d287104f40b7cfcb43b8d7f8b52b";
   path = makeBinPath ([ mono python ] ++ optional (zenity != null) zenity);
   rpath = makeLibraryPath [ lua openal SDL2 ];
@@ -22,8 +22,8 @@ in stdenv.mkDerivation rec {
     (fetchFromGitHub {
       owner = "IceReaper";
       repo = "KKnD";
-      rev = "82505807c9fffd993bb2cb4ecbb1e292e4ca0b18";
-      sha256 = "0wh7j8k1rkjyqfnligd80r0abf88x5wp5qfw9zn26jrlxmypyzl8";
+      rev = "fcf0f93e7d5cdb4e7884c7dcb8bca51486c60a15";
+      sha256 = "0ncdhgn9qwa2ca8pkhyy8q4jb8g2nig793i7nvxygbnh2lb233xg";
       name = "KKnD";
     })
     (fetchFromGitHub {
