9a8894aedfea1f4e1c332041a93aa9e05e5796c4
Brenton Horne <brentonhorne77@gmail.com>
2019-01-03 15:41:41 +1000

    openra-yr: :arrow_up: 115

--- pkgs/games/openra-yr/default.nix
+++ pkgs/games/openra-yr/default.nix
@@ -10,7 +10,7 @@ with stdenv.lib;
 
 let
   pname = "openra-yr";
-  version = "114";
+  version = "115";
   engine-version = "release-20180923";
   path = makeBinPath ([ mono python ] ++ optional (zenity != null) zenity);
   rpath = makeLibraryPath [ lua openal SDL2 ];
@@ -22,8 +22,8 @@ in stdenv.mkDerivation rec {
     (fetchFromGitHub {
       owner = "cookgreen";
       repo = "yr";
-      rev = "a9150114808dc2823b47b97e39969bdb6b27cf98";
-      sha256 = "1bjagkbqlkw6djxv86h1wfhnj6n2ggmfd5qp1b444inxblgk8wrf";
+      rev = "29db0f1b0135d0cae309442a001b4271997da98b";
+      sha256 = "0rdf377pk2fyi96cxayqx8dj345kir9r7a8bpfc44grbpncbafcl";
       name = "yr";
     })
     (fetchFromGitHub {
