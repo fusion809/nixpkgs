9f3795876ac0f34dc28e65e542de2cb43f4831fa
Brenton Horne <brentonhorne77@gmail.com>
2019-01-29 02:32:43 +1000

    openra-gen: :arrow_up: 1151.

--- pkgs/games/openra/mods.nix
+++ pkgs/games/openra/mods.nix
@@ -84,23 +84,23 @@ in {
   };
 
   gen = buildOpenRAMod {
-    version = "1150";
+    version = "1151";
     title = "Generals Alpha";
     description = "Re-imagination of the original Command & Conquer: Generals game";
     homepage = https://github.com/MustaphaTR/Generals-Alpha;
     src = fetchFromGitHub {
       owner = "MustaphaTR";
       repo = "Generals-Alpha";
-      rev = "62bbd89d0a1f0e5e9ba300f1cbded90f793d9421";
-      sha256 = "1spc62izm0znmi0q18bmf842j7y32akk10cc47125qxhfd5fvbrs";
+      rev = "e29f09b7a8483440606678121a3c00e845806cab";
+      sha256 = "1bsq9l9j2i9qgd1aik1ah2i4379vgqbfy0z26irzhp71dylycs25";
     };
     engine = rec {
-      version = "gen-20190128";
+      version = "gen-20190128_2";
       src = fetchFromGitHub {
         owner = "MustaphaTR";
         repo = "OpenRA" ;
         rev = version;
-        sha256 = "0hjyg7xyf6rw2rnylxbl9742g9c7fqwnyfqzj87vsrmr8rhawl0d";
+        sha256 = "1zyqh8y8rqpj95d92hdc162w5813d3rbihynj37kll212pwc3gg3";
         name = "generals-alpha-engine";
         inherit extraPostFetch;
       };
