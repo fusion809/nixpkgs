0296c65ca6922e5c0c7378a70f8aba2b63a27c5a
Brenton Horne <brentonhorne77@gmail.com>
2019-07-23 14:01:14 +1000

    openra: :arrow_up: 26275 (d9e1a68)

--- pkgs/games/openra/engines.nix
+++ pkgs/games/openra/engines.nix
@@ -33,9 +33,9 @@ in {
     sha256 = "1vqvfk2p2lpk3m0d3rpvj34i8cmk3mfc7w4cn4llqd9zp4kk9pya";
   };
 
-  bleed = let commit = "9ac3d7507c8c032ecb8ea986997d66b97528075b"; in buildUpstreamOpenRAEngine {
-    version = "26274.git.9ac3d75";
+  bleed = let commit = "d9e1a6845352f041144fda156491abed1e1b227f"; in buildUpstreamOpenRAEngine {
+    version = "26275.git.d9e1a68";
     rev = commit;
-    sha256 = "1mv1j2h8v365lb01fa796gwqryx5ifh6px3wa9mis93zhnrpgsml";
+    sha256 = "00xf12xdqxhm4szbd9vwc0q6lwsgl3dq1k7a4kahk5vlmjrdyvmr";
   };
 }
