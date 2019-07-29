58cfcaf349169e8e48f9364d98900086ad8c383f
Brenton Horne <brentonhorne77@gmail.com>
2019-07-29 17:21:38 +1000

    openra-rv: :arrow_up: 1943.

--- pkgs/games/openra/mods.nix
+++ pkgs/games/openra/mods.nix
@@ -208,15 +208,15 @@ in {
   };
 
   rv = unsafeBuildOpenRAMod {
-    version = "1941";
+    version = "1943";
     title = "Romanov's Vengeance";
     description = "Re-imagination of the original Command & Conquer: Red Alert 2 game";
     homepage = https://github.com/MustaphaTR/Romanovs-Vengeance;
     src = fetchFromGitHub {
       owner = "MustaphaTR";
       repo = "Romanovs-Vengeance";
-      rev = "84ee8d317eeedf2eb5470ef89dc0cc411149f6ff";
-      sha256 = "0yawwcsgi8qyn4acl3kapzz0c2w13jqz7cx45gxk37517snyja49";
+      rev = "ccab3605b952b14c4439851c4a968b8fd4b5225b";
+      sha256 = "1dznn95kk1s0kvfcwgyj41afk7283lvhnnp3a8fxnzg17j0pwqi5";
     };
     engine = let commit = "d16e9ff"; in {
       version = abbrevCommit commit;
