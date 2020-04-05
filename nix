c7091857fd9daf85af0569b140e01b420821f551
Brenton Horne <brentonhorne77@gmail.com>
2020-04-06 00:15:22 +1000

    vim: :arrow_up: 8.2.0511.

--- nix
+++
@@ -1,1 +0,0 @@
-/tmp/nixos-rebuild.sqVncL/nix
--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "8.2.0510";
+  version = "8.2.0511";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "16ggwf238pzzkn3ng6n09qz9cgdzha9vc0ghlcxhwilnjh12l7p9";
+    sha256 = "02x7fg58hr5dy6acyrj8kh3d00y7cv1590v9m3iwphv7fpzcr1rc";
   };
 
   enableParallelBuilding = true;
