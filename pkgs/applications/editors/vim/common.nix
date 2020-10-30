e1e5a28bb8b34dd848633b70e00433ba4c634796
Brenton Horne <brentonhorne77@gmail.com>
2020-10-31 04:25:32 +1000

    vim: :arrow_up: 8.2.1926.

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "8.2.1923";
+  version = "8.2.1925";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "0asv9zz8k7c39qqaybwssmlfhwmrnjk6chdpd3lxq0iy1aczfbcj";
+    sha256 = "0cbvbja4wkgd4z9y8wcy46gi7sxhk3a3zfv2glp073rjcippdp75";
   };
 
   enableParallelBuilding = true;
